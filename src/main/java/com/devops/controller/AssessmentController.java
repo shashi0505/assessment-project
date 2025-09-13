13919e8e828c2b37b8e3e8c1b58e7a3eb58de635
shashi0505 <61519339+shashi0505@users.noreply.github.com>
2025-09-13 23:38:37 +0530

    rest designed

---
+++ src/main/java/com/devops/controller/AssessmentController.java
@@ -0,0 +1,15 @@
+package com.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AssessmentController {
+
+    @GetMapping("msg")
+    public String getAssessment (){
+        return "This is my first Assessment";
+    }
+}
